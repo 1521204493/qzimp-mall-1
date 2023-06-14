8a181770dfbfcf8ff146185a026625268b32c96e
fangjingtao <1521204493@qq.com>
2023-06-14 10:45:24 +0800

    mall

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/qzimp-mall-1",
   plugins: [
     vue(), 
     vueJsx(),
@@ -19,6 +20,9 @@ export default defineConfig({
     }),
     
   ],
+  build: {
+    outDir: "docs"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
